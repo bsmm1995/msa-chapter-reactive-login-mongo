25b69e2d42559a4f3c16daf12ac4088f98cb306b
Bladimir Minga <bminga@pegasus.cl>
2023-12-24 15:55:12 -0500

    Refactor

--- src/main/java/com/bsmm/login/Application.java
+++ src/main/java/com/bsmm/login/Application.java
@@ -3,13 +3,19 @@ package com.bsmm.login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
