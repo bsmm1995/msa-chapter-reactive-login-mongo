044944c8163b08144ea053d04536b0f267ba95a7
Bladimir Minga <bminga@pegasus.cl>
2024-01-03 12:27:52 -0500

    Refactoore redis copnnection

--- src/main/java/com/bsmm/login/config/RedisConfig.java
+++ src/main/java/com/bsmm/login/config/RedisConfig.java
@@ -6,9 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -38,13 +35,5 @@ public class RedisConfig {
         RedisSerializationContext<String, LoginResponse> context = builder.value(serializer).build();
 
         return new ReactiveRedisTemplate<>(factory, context);
-    }
-
-    @Bean
-    @Primary
-    public ReactiveRedisConnectionFactory connectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
-        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 }
